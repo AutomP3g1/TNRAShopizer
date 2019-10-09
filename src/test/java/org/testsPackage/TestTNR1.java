e6e0769d920463e62884a71c5c6ca33cc3b60075
AutomP3g1 <56156366+AutomP3g1@users.noreply.github.com>
2019-10-09 20:35:40 +0200

    xc modifé

--- src/test/java/org/testsPackage/TestTNR1.java
+++ src/test/java/org/testsPackage/TestTNR1.java
@@ -10,6 +10,11 @@ import java.util.List;
 import org.junit.Test;
 import org.myPagesObject.HomePage;
 import org.myPagesObject.PageTable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestTNR1 extends GenericTest {
 
@@ -23,83 +28,59 @@ public class TestTNR1 extends GenericTest {
 
 	@Test
 	public void testTable() throws InterruptedException, FileNotFoundException {
-		
+
 		// pas 1 : Ouvrir un navigateur et accéder à l'application Shopizer
 		HomePage homePage = new HomePage(driver);
 
 		// pas 2 : Selectionner dans le menu horizontal la partie "Table"
-		
+
 		PageTable pageTable = homePage.clickBandeauTable();
 		Thread.sleep(1000);
 		assertTrue(pageTable.userPath.isDisplayed()); // une arboresence apparait sous le titre de la page indiquant le chemin ou l'utilsateur se situe
 
-		// pas 3 : Verifier la présence des éléments:
-			/*
+		// pas 3 : Veifier la présence des éléments:
+		/*
 			 	- Natural Root Console
 				- Asian Rosewood Console
 				- Edge Console
 				- CoffeTable Accacia
-			 */
-		
+		 */
+
 		//Thread.sleep(1000);
 		myProductLists = pageTable.WebElementToArray(pageTable.tablePageProductElements);
 		myJDDLists = pageTable.loadFile("src/test/resources/tablePageProductsElements.txt");
 		assertEquals(myJDDLists, myProductLists);
-		
-		
+
+
 		// pas 4 : Vérifier le prix actuel de tout les éléments
-			/*
+		/*
 			 	Natural Root Console 1499.99 USD
 				Asian Rosewood Console   499.99 USD
 				Edge Console 749.99 USD
 				CoffeTable Accacia 399.99 USD
-			 */
+		 */
 		myPriceLists = pageTable.WebElementToArray(pageTable.tablePagePriceElements);
 		myJDDLists = pageTable.loadFile("src/test/resources/tablePagePriceElements.txt");
-		assertEquals(myJDDLists, myPriceLists);
-		
-		// pas 5 : Selectionner le filtre DEFAULT : il n'y a plus que l'es éléments :Asian Rosewood Console et Edge Console; de présent.
-		pageTable.filtreDefault.click();
-		Thread.sleep(1000);
-		myProductLists = pageTable.WebElementToArray(pageTable.tablePageProductElements);
-		myJDDLists = pageTable.loadFile("src/test/resources/tablePageFiltreDEFAULTProducts.txt");
-		assertEquals(myJDDLists, myProductLists);
 
+		//pageTable.CheckProductsPrices();
+
+		// pas 5 : Selectionner le filtre DEFAULT : il n'y a plus que l'es éléments :Asian Rosewood Console et Edge Console; de présent.
+
+		pageTable.filtreDefault.click(); 
+		Thread.sleep(1000);
+		assertEquals("Asian Rosewood Console", pageTable.productDefault1.getText());
+		assertEquals("Edge Console", pageTable.productDefault2.getText());
 		
+
 		// pas 6 : Selectionner le filtre Asian wood : il n'y a plus que l'éléments Coffee Table Accacia de présent.
 		pageTable.filtreAsianWood.click();
 		Thread.sleep(1000);
-		myProductLists = pageTable.WebElementToArray(pageTable.tablePageProductElements);
-		myJDDLists = pageTable.loadFile("src/test/resources/tablePageFiltreAsianWoodProducts.txt");
-		assertEquals(myJDDLists, myProductLists);
-		
-		
+		assertEquals("Coffee Table Accacia", pageTable.productCoffeeTable.getText());
+
 		// pas 7 : Selectionner le filtre Roots : il n'y a plus que l'élément Natural Root Console de présent.
 		pageTable.filtreRoots.click(); //pageTable.locateElement(pageTable.filtreRoots);
 		Thread.sleep(1000);
-		myProductLists = pageTable.WebElementToArray(pageTable.tablePageProductElements);
-		myJDDLists = pageTable.loadFile("src/test/resources/tablePageFiltreRootsProducts.txt");
-		assertEquals(myJDDLists, myProductLists);
-		
+		assertEquals("Natural Root Console", pageTable.productNaturalRootConsole.getText());
+
 	}
-	// @Test
-	// public void testTable() throws InterruptedException {
-
-//		HomePage homePage = new HomePage(driver);
-//
-//		homePage.clickBandeauTable();
-//
-//		PageTable pageTable = new PageTable(driver);
-//
-//		//pageTable.locateElement(pageTable.filtreDefault);
-//		pageTable.filtreDefault.click();
-//		Thread.sleep(1000);
-//		assert
-//		//(pageTable.productTitles.isDisplayed());
-//
-//		
-//		pageTable.locateElement(pageTable.filtreRoots);
-//
-	// }
-
 }
