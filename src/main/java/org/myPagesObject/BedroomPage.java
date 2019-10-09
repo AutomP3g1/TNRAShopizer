34a13887974e07359fb00469020650332c575f10
AutomP3g1 <56156366+AutomP3g1@users.noreply.github.com>
2019-10-09 11:02:01 +0200

    update bedroom page 09/10

--- src/main/java/org/myPagesObject/BedroomPage.java
+++ src/main/java/org/myPagesObject/BedroomPage.java
@@ -12,13 +12,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BedroomPage {
 	
 	public BedroomPage addToCart (WebDriver driver) throws InterruptedException {
-		driver.findElement(By.xpath("(//a[@class='addToCart'])[1]")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//a[@class='addToCart'])[2]")).click();
+		Thread.sleep(3000);
+		WebDriverWait wait1 = new WebDriverWait(driver, 10);
+		WebElement element2 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[text()='Ajouter au panier'])[1]")));
+		Thread.sleep(3000);
+		element2.click();
+		Thread.sleep(3000);
+//		driver.findElement(By.xpath("(//a[@class='addToCart'])[1]")).click();
+		WebDriverWait wait2 = new WebDriverWait(driver, 10);
+		WebElement element3 = wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[text()='Ajouter au panier'])[2]")));
+		Thread.sleep(3000);
+		element3.click();
+//		driver.findElement(By.xpath("(//a[@class='addToCart'])[2]")).click();
+		
 		return PageFactory.initElements(driver,BedroomPage.class);
 	}
 	
 	public BedroomPage mouseOverCart (WebDriver driver) throws InterruptedException {
+		Thread.sleep(3000);
 		Actions action = new Actions(driver);
 		WebElement we = driver.findElement(By.xpath("//span[@class='lnr lnr-cart']"));
 		action.moveToElement(we).build().perform();
